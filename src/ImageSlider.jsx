4848e99ae4df10a00c4dbad159181e2c18f039f2
Zaw Khing <zawmyokhing@gmail.com>
2022-12-29 22:39:35 +0000

    Trying to do image carousel

--- src/ImageSlider.jsx
+++ src/ImageSlider.jsx
@@ -1,8 +1,8 @@
 
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css"
-import React from 'react'
+import "slick-carousel/slick/slick-theme.css";
+import React from 'react';
 
 const ImageSlider = ({images}) => {
 
@@ -13,9 +13,9 @@ const ImageSlider = ({images}) => {
    slidesToScroll: 1,
    lazyLoad: true,
    autoplay: true,
-   autoplaySpeed: 2000,
+   autoplaySpeed: 3000,
   };
-  
+
   return (
    <>
     <div className="imgslider">
--- src/images.js
+++ src/images.js
@@ -1,18 +1,17 @@
-
 const images = [
   {
     id: 1,
-    src: "./images/Img 1.png",
+    src: "https://images.unsplash.com/photo-1672318995271-264173219d70?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxNXx8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=60",
     alt: "Image 1",
   },
   {
     id: 2,
-    src: "./images/Img 2.png",
+    src: "https://images.unsplash.com/photo-1672106157109-056c567e3833?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw4fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=600&q=60",
     alt: "Image 2 ",
   },
   {
     id: 3,
-    src: "./images/Img 3.png",
+    src: "https://images.unsplash.com/photo-1672318042589-20f40aa2f135?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxOHx8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=60",
     alt: "Image 3",
   },
 ];
