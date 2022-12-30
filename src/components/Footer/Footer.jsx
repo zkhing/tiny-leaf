3111e92a1a44193cdaf12a9de262f639a7a3d7e4
Zaw Khing <zawmyokhing@gmail.com>
2022-12-30 17:39:02 +0000

    Responsive image with text and button

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import logo from '../../images/Ellipse 4.svg'
+import logo from '../../images/Ellipse 4.svg';
+import social from '../../images/Social.png';
 
 function Footer() {
   return (
@@ -9,15 +10,17 @@ function Footer() {
 
       <div className='footer-info'>
       <h3>Opening hours</h3>
-         <p>Mon - Sat 11am - 12pm 
+         <p>Mon - Sat 11am - 12pm<br></br>
             Sun 11am - 11pm</p>
       <h3>Address</h3>
-         <p>24 Brew Lane
+         <p>24 Brew Lane<br></br>
             BR3 WL, CupTown</p>
       <h3>Contact</h3>
-         <p>+44 12345 67890
+         <p>+44 12345 67890<br></br>
             hello@yellowmountain.com</p>
       </div>
+
+      <img className='social-logo' src={social} alt=''/>
 
     </div>
   );
--- src/components/Reservation/MakeReservation.jsx
+++ src/components/Reservation/MakeReservation.jsx
@@ -5,9 +5,11 @@ function MakeReservation() {
   return (
     <div className="reservation-container">
       <img className="reservationImg" src={image} alt="" />
-      <h3 className="first-txt">Make a reservation</h3>
-      <p className="second-txt">For anything from a catchup with a long lost friend to a book club gathering... we're always ready to put the kettle on.</p>
-      <button className="btn-reservation">Reserve a table</button>
+      <div className="caption">
+         <h3 className="first-txt">Make a reservation</h3>
+         <p className="second-txt">For anything from a catchup with a long lost friend to a book club gathering... we're always ready to put the kettle on.</p>
+         <button className="btn-reservation">Reserve a table</button>
+      </div>
     </div>
   );
 }
