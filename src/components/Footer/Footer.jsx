71915005628e8bfe96f94076d58175d22a26263b
Zaw Khing <zawmyokhing@gmail.com>
2023-01-12 13:15:22 +0000

    Refactor on footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,42 +5,32 @@ import social from '../../images/Social.png';
 function Footer() {
   return (
     <div className="footer-container">
-
       <div className="footer-logo">
         <img src={logo} alt="Logo" />
         <h2>Tiny Leaf Tea House</h2>
       </div>
 
       <div className="footer-info">
-        <div>
+        <div className="footer-text">
           <h4>Opening hours</h4>
-          <p>
-            Mon - Sat 11am - 12pm<br></br>
-            Sun 11am - 11pm
-          </p>
+          <p>Mon - Sat 11am - 12pm 
+            Sun 11am - 11pm</p>
         </div>
 
-        <div>
+        <div className="footer-text">
           <h4>Address</h4>
-          <p>
-            24 Brew Lane<br></br>
-            BR3 WL, CupTown
-          </p>
+          <p>24 Brew Lane BR3 WL, CupTown</p>
         </div>
 
-        <div>
+        <div className="footer-text">
           <h4>Contact</h4>
-          <p>
-            +44 12345 67890<br></br>
-            hello@yellowmountain.com
-          </p>
+          <p>+44 12345 67890 hello@yellowmountain.com</p>
         </div>
       </div>
 
       <div className="social-logo">
-        <img src={social} alt="" />
+        <img src={social} alt="Social Media" />
       </div>
-
     </div>
   );
 }
--- src/components/Home/About/WiseLeafEvenings.jsx
+++ src/components/Home/About/WiseLeafEvenings.jsx
@@ -6,6 +6,8 @@ function WiseLeafEvenings() {
   return (
     <>
       <section className="wiseleaf-container">
+
+        <div className="greenbox" ></div>
         <div className="wiseleaf-img1">
           <img className="teapot-img" src={img1} alt="teapot" />
         </div>
